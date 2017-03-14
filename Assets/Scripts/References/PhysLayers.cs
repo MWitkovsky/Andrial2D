5966eaabf0a0a6bd86a13c6b0aaae86905dca6dd
Michael Witkovsky <Michael.Witkovsky@gmail.com>
2017-03-14 13:35:15 -0400

    Added phys layer and tag references

---
+++ Assets/Scripts/References/PhysLayers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysLayers : MonoBehaviour {
+
+    public static readonly string defaultLayer = "Default";
+    public static readonly string transparentFX = "Transparent FX";
+    public static readonly string ignoreRaycast = "Ignore Raycast";
+    public static readonly string water = "Water";
+    public static readonly string ui = "UI";
+    public static readonly string ground = "Ground";
+}
---
+++ Assets/Scripts/References/Tags.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tags : MonoBehaviour {
+
+    public static readonly string untagged = "Untagged";
+    public static readonly string respawn = "Respawn";
+    public static readonly string finish = "Finish";
+    public static readonly string editorOnly = "EditorOnly";
+    public static readonly string mainCamera = "MainCamera";
+    public static readonly string player = "Player";
+    public static readonly string gameController = "GameController";
+
+}
